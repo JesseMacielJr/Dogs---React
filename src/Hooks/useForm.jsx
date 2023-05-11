cc1b94ac4e05b9562d151ef97020511ead5bbe28
Jesse Junior <jessejuniormaciel@hotmail.com>
2023-05-11 17:10:29 -0300

    aula 0809 - API Fetch

---
+++ src/Hooks/useForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  // Caso eu retornasse apenas "validate" então lá no loginForm.js eu deveria usar, por exemplo, password.validate(password)
+  // Mas como já estou ativando a função usando o próprio value como argumento então é só usar password.validate()
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
