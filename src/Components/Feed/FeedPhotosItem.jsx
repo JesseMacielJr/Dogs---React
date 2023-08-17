745df009cd9d423664cacef04672ce521c69e73a
Jesse Junior <jessejuniormaciel@hotmail.com>
2023-08-17 20:59:43 -0300

    aula 0825 - Image Skeleton

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import styles from './FeedPhotosItem.module.css';
+import React from "react";
+import styles from "./FeedPhotosItem.module.css";
+import Image from "../Helper/Image";
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
-
   function handleClick() {
     setModalPhoto(photo);
   }
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image alt={photo.title} src={photo.src} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
