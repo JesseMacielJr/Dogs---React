787a316e7cfb56a089dd74e496710b0c21684fce
Jesse Junior <jessejuniormaciel@hotmail.com>
2023-08-30 11:03:26 -0300

    aula 0827 - Scroll Infinito

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import FeedPhotosItem from './FeedPhotosItem';
-import useFetch from '../../Hooks/useFetch';
-import { PHOTOS_GET } from '../../api';
-import Error from '../Helper/Error';
-import Loading from '../Helper/Loading';
-import styles from './FeedPhotos.module.css';
+import React from "react";
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 }); // "user: 0" puxa de qualquer usuário
+      const total = 6;
+      const { url, options } = PHOTOS_GET({
+        page,
+        total,
+        user,
+      }); // "user: 0" puxa de qualquer usuário
       const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) setInfinite(false);
       console.log(json);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
@@ -24,7 +30,11 @@ const FeedPhotos = ({ setModalPhoto }) => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
